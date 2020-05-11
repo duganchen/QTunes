11140d634a805448502933e55533f1657b29ff37
Dugan Chen <thedoogster@gmail.com>
2020-05-10 17:10:21 -0700

    Implement the Rule of Five for mpdsong

--- src/mpdsong.cpp
+++ src/mpdsong.cpp
@@ -4,7 +4,38 @@ MPDSong::MPDSong(mpd_song *song, QObject *parent) : AbstractMPDSong(parent), m_s
 {
 }
 
+bool MPDSong::isNull()
+{
+    return m_song == nullptr;
+}
+
+MPDSong::MPDSong(const MPDSong &other)
+{
+    m_song = mpd_song_dup(other.m_song);
+}
+
+MPDSong::MPDSong(MPDSong &&other) : m_song{other.m_song}
+{
+    other.m_song = nullptr;
+}
+
+MPDSong &MPDSong::operator=(const MPDSong &other)
+{
+    m_song = mpd_song_dup(other.m_song);
+    return *this;
+}
+
+MPDSong &MPDSong::operator=(MPDSong &&other)
+{
+    m_song = other.m_song;
+    other.m_song = nullptr;
+    return *this;
+}
+
 MPDSong::~MPDSong()
 {
-    mpd_song_free(m_song);
+    if (m_song)
+    {
+        mpd_song_free(m_song);
+    }
 }
