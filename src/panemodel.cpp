69d62ca60c716d489f7b87d21d10b786250ff961
Dugan Chen <thedoogster@gmail.com>
2020-05-15 08:47:51 -0700

    Remove debug-logging

--- src/panemodel.cpp
+++ src/panemodel.cpp
@@ -3,46 +3,45 @@
 #include <QtAlgorithms>
 
 PaneModel::PaneModel(const QVector<AbstractItem *> &list, ItemModelController *controller, QObject *parent)
-	: QAbstractListModel(parent), m_list(list)
+    : QAbstractListModel(parent), m_list(list)
 {
-	controller->setParent(this);
-	QObject::connect(controller, &ItemModelController::aboutToBeReset, this, &PaneModel::beginResetModel);
-	QObject::connect(controller, &ItemModelController::reset, this, &PaneModel::endResetModel);
-	QObject::connect(controller, &ItemModelController::rowsAboutToBeRemoved, this, &PaneModel::beginRemoveRows);
-	QObject::connect(controller, &ItemModelController::rowsRemoved, this, &PaneModel::endRemoveRows);
+    controller->setParent(this);
+    QObject::connect(controller, &ItemModelController::aboutToBeReset, this, &PaneModel::beginResetModel);
+    QObject::connect(controller, &ItemModelController::reset, this, &PaneModel::endResetModel);
+    QObject::connect(controller, &ItemModelController::rowsAboutToBeRemoved, this, &PaneModel::beginRemoveRows);
+    QObject::connect(controller, &ItemModelController::rowsRemoved, this, &PaneModel::endRemoveRows);
 }
 
 int PaneModel::rowCount(const QModelIndex &parent) const
 {
-	qDebug() << "Calling rowCount";
     // For list models only the root node (an invalid parent) should return the list's size. For all
     // other (valid) parents, rowCount() should return 0 so that it does not become a tree model.
-	if (parent.isValid())
+    if (parent.isValid())
         return 0;
 
-	return m_list.size();
+    return m_list.size();
 }
 
 QVariant PaneModel::data(const QModelIndex &index, int role) const
 {
     Q_UNUSED(role)
 
-	if (!index.isValid())
+    if (!index.isValid())
     {
         return QVariant();
     }
 
-	if (m_list.size() - 1 < index.row())
+    if (m_list.size() - 1 < index.row())
     {
         return QVariant();
     }
 
-	return m_list.at(index.row())->data();
+    return m_list.at(index.row())->data();
 }
 
 QHash<int, QByteArray> PaneModel::roleNames() const
 {
     QHash<int, QByteArray> roles;
     roles[Qt::DisplayRole] = "value";
-	return roles;
+    return roles;
 }
